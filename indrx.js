b2d6186e56afd093e335b204f4d997f708fb53a0
yasminrodr <142456295+yasminrodr@users.noreply.github.com>
2023-08-22 21:47:54 -0300

    Create indrx.js

---
+++ indrx.js
@@ -0,0 +1,36 @@
+let variavelTexto =  "texto";
+let variavelNumero = 6;
+console.log(typeof variavelTexto);
+console.log(typeof variavelNumero);
+
+//variavel vazia
+let variavelVazia;
+//console.log(typeof variavelVazia);
+let variavelNula = null;
+//console.log(typeof variavelNula);
+
+let a = "b";
+console.log(a);
+console.log(typeof a);
+
+
+//exercicio 2
+
+let nome; 
+let idade;
+
+let numero = 6
+
+nome = prompt("Qual é seu nome?");
+idade =prompt("Qual a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(numero + 5);
+console.log(idade.toString()+5);
+
+//conversao numero
+let n = "42";
+console.log(Number(n));
+console.log(typeof n );
+console.log(typeof Number(n));
+
