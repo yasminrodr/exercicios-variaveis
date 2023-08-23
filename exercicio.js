091d4f414bc56c5ec94085ad4473cdb7a7de019a
yasminrodr <142456295+yasminrodr@users.noreply.github.com>
2023-08-22 21:45:58 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,20 @@
+/* 
+*Sara Yasmin
+*
+*/
+//1. b =10; a =10 b =5;
+
+//2. c = a-10; b = c-10; a = b-10;
+
+//3. a) variavel p = horas trabalhadas
+//3. b) variavel t = diaria recebida
+
+// tarefa de codigo
+let nome;
+let idade;
+console.log(typeof nome);
+console.log(typeof idade);
+// nao foi atribuida valor
+
+
+ 
